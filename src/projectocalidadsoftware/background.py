f8c753b1653a773c9776d687d83397da6f5d3b17
Lorenzo Rodriguez <lrodriguezv-m@ucenfotec.ac.cr>
2025-11-17 20:20:41 -0600

    test method test_is_ready refactor

--- src/projectocalidadsoftware/background.py
+++ src/projectocalidadsoftware/background.py
@@ -1,6 +1,7 @@
 import nest_asyncio, threading, time, random, os, statistics, asyncio
+from typing import Optional
 
-def is_ready(url: str | None) -> bool:
+def is_ready(url: Optional[str]) -> bool:
     return bool(url) and isinstance(url, str) and url.startswith("http")
 
 
