1f57bfbdcd1d8643b0abcb1c6321cc680b7d859b
Lorenzo Rodriguez <lrodriguezv-m@ucenfotec.ac.cr>
2025-11-17 20:22:32 -0600

    test method test_is_ready refactor

--- src/projectocalidadsoftware/main.py
+++ src/projectocalidadsoftware/main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
 import threading
 
-from src.projectocalidadsoftware.background import start_background_loop
 from src.projectocalidadsoftware.health import get_health_status
 
 app = FastAPI(title="Defense System")
-threading.Thread(target=start_background_loop, daemon=True).start()
 
 @app.get("/health/status")
 def health_status():
