8d24133a1dd489c05fef0c202904c3c74e4b05e6
Lorenzo Rodriguez <lrodriguezv-m@ucenfotec.ac.cr>
2025-11-17 20:16:44 -0600

    test method test_is_ready refactor

--- src/projectocalidadsoftware/background.py
+++ src/projectocalidadsoftware/background.py
@@ -1,17 +1,6 @@
-import time
+import nest_asyncio, threading, time, random, requests, os, statistics, asyncio
 
-_last_validation_ok = True
+def is_ready(url: str | None) -> bool:
+    return bool(url) and isinstance(url, str) and url.startswith("http")
 
-# TODO: Usar este metodo para anadir la logica para defender la colonia
-def defend_colony():
-    global _last_validation_ok
-    print("Checking for attacks to the colony...")
-    _last_validation_ok = True
 
-def start_background_loop():
-    while True:
-        defend_colony()
-        time.sleep(10)
-
-def get_validation_status():
-    return _last_validation_ok
